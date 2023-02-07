639c864bf3e1d5baf726c4188a2222eaf762f693
Balázs Kerekes <kerekes.balazs.12@gmail.com>
2023-02-07 18:48:32 +0100

    create tests for service center

--- test/Szerviz.test.js
+++ test/Szerviz.test.js
@@ -6,6 +6,11 @@ const web3 = new Web3(ganache.provider());
 const compiledFactory = require("../ethereum/build/AdattaroloFactory.json");
 const compiledSzerviz = require("../ethereum/build/Szerviz.json");
 
+const originalCim = "cim";
+const originalGPS = "gps";
+const originalEmail = "email";
+const originalNyitvatartas = "nyitvatartas";
+
 let accounts;
 let factory;
 let campaignAddress;
@@ -18,7 +23,7 @@ beforeEach(async () => {
         .deploy({ data: compiledFactory.evm.bytecode.object })
         .send({ from: accounts[0], gas: "5000000" });
 
-    await factory.methods.createSzerviz("cim","gps","email", "nyitvatartas").send({
+    await factory.methods.createSzerviz(originalCim, originalGPS, originalEmail, originalNyitvatartas).send({
         from: accounts[0],
         gas: "5000000",
     });
@@ -27,7 +32,7 @@ beforeEach(async () => {
     szerviz = await new web3.eth.Contract(compiledSzerviz.abi, campaignAddress);
 });
 
-describe("Szerviz", () => {
+describe("Service center", () => {
     it("deploys a factory and a service center", () => {
         assert.ok(factory.options.address);
         assert.ok(szerviz.options.address);
@@ -47,14 +52,16 @@ describe("Szerviz", () => {
             ujCim);
     });
 
-    it("can't modify address as not manager", async () => {
+    it("only manager can change address", async () => {
+        let msg = "";
+
         try {
             await szerviz.methods.setCim("asdf").send({from: accounts[1]});
-            assert.strictEqual("", "adsf");
         } catch (err) {
-            console.log(err);
-            assert.strictEqual(true, true);
+            msg = err.message;
         }
+
+        assert.notEqual(msg, "");
     });
 
     it("can modify gps as manager", async () => {
@@ -71,6 +78,18 @@ describe("Szerviz", () => {
             ujGps);
     });
 
+    it("only manager can change gps", async () => {
+        let msg = "";
+
+        try {
+            await szerviz.methods.setGPS("asdf").send({from: accounts[1]});
+        } catch (err) {
+            msg = err.message;
+        }
+
+        assert.notEqual(msg, "");
+    });
+
     it("can modify email as manager", async () => {
         const email = await szerviz.methods.Email().call();
         const ujEmail = "masikCim";
@@ -83,6 +102,18 @@ describe("Szerviz", () => {
         assert.strictEqual(
             await szerviz.methods.Email().call(),
             ujEmail);
+    });
+
+    it("only manager can change email", async () => {
+        let msg = "";
+
+        try {
+            await szerviz.methods.setEmail("asdf").send({from: accounts[1]});
+        } catch (err) {
+            msg = err.message;
+        }
+
+        assert.notEqual(msg, "");
     });
 
     it("can modify open hours as manager", async () => {
@@ -99,7 +130,25 @@ describe("Szerviz", () => {
             ujOpenHours);
     });
 
-    it("can get summary as manager", () => {
+    it("only manager can change open hours", async () => {
+        let msg = "";
 
+        try {
+            await szerviz.methods.setNyitvatartas("asdf").send({from: accounts[1]});
+        } catch (err) {
+            msg = err.message;
+        }
+
+        assert.notEqual(msg, "");
+    });
+
+    it("can get summary as manager", async () => {
+        let msg = await szerviz.methods.getSummary().call();
+
+        assert.strictEqual(msg[0], accounts[0]);
+        assert.strictEqual(msg[1], originalCim);
+        assert.strictEqual(msg[2], originalGPS);
+        assert.strictEqual(msg[3], originalEmail);
+        assert.strictEqual(msg[4], originalNyitvatartas);
     });
 });
